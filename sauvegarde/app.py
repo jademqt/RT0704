429e47c79ff91a65ec3e60853e0a49dad6ea745b
jademqt <113434437+jademqt@users.noreply.github.com>
2022-11-09 18:27:46 +0100

    API flask début
    le copie/collé était mal passé donc new commit sans les pb de syntaxes du copié précédent

--- sauvegarde/app.py
+++ sauvegarde/app.py
@@ -1,17 +1,7 @@
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello world !\n"
-
-@app.route('/index/')
-def test_index():
-    return render_template("index.html")
-
 @app.route('/api/test/')
 def test():
-    #convertir un dictionnaire en un JSON puis le renvoie sous forme de réponse HTTP    dictionnaire = {
+    #convertir un dictionnaire en un JSON puis le renvoie sous forme de réponse HTTP
+    dictionnaire = {
         'title': 'La nuit du 12',
         'director': 'Dominik Moll',
         'year': 2021,
@@ -20,7 +10,9 @@ def test():
     return jsonify(dictionnaire)
 
     """
-    Qd le client appellera l'endpoint /api/test/ le programme Python enverra une re>    On stocke la réponse de REST dans la variable response, on les convertit en dic>    si elle fonctionne renvoie 200
+    Qd le client appellera l'endpoint /api/test/ le programme Python enverra une requête vers l'API REST (A FAIRE)
+    On stocke la réponse de REST dans la variable response, on les convertit en dictionnaire Python grâce à json.loads
+    si elle fonctionne renvoie 200
     """
     #response = requests.get(VIDEO_API_URL)
     #content = json.loads(response.content.decode('utf-8'))
@@ -28,5 +20,9 @@ def test():
     if response.status_code != 200:
         return jsonify({
             'status': 'error',
-            'message': 'La requête à l\'API REST(?) n\'a pas fonctionné. Voici le m>        }), 500
+            'message': 'La requête à l\'API REST(?) n\'a pas fonctionné. Voici le message renvoyé par l\'API : {}'.format(content['message'])
+        }), 500
     """
+
+if __name__ == "__main__":
+    app.run(debug=True)
