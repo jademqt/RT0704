18d073503d0bc484ee0cbf421da6186e655c7e85
jademqt <113434437+jademqt@users.noreply.github.com>
2022-11-19 19:54:35 +0100

    get essais....

--- web/app.py
+++ web/app.py
@@ -47,39 +47,15 @@ def import_movies():
 def import_movies():
     return render_template('explore_videolib.html')
 
-'''
-@app.route('/templates/')
+
+@app.route('/actor_created')
 def import_actors():
     first_name = request.form['import_firstname']
     last_name = request.form['import_lastname']
-    print("First name : ", first_name, " et last name ", last_name)
-    return 'Submitted!'
-'''
+    return render_template('actor_created.html')
+    return first_name + " " + last_name
 
-def test():
-    #convertir un dictionnaire en un JSON puis le renvoie sous forme de reponse HTTP
-    dictionnaire = {
-        'title': 'La nuit du 12',
-        'director': 'Dominik Moll',
-        'year': 2021,
-        'actors': ["Bastion Bouillon", "Theo Cholbi", "Julien Frison"]
-    }
-    return jsonify(dictionnaire)
 
-    """
-    Qd le client appellera l'endpoint /api/test/ le programme Python enverra une requete vers l'API REST (A FAIRE)
-    On stocke la reponse de REST dans la variable response, on les convertit en dictionnaire Python grace a json.loads
-    si elle fonctionne renvoie 200
-    """
-    #response = requests.get(VIDEO_API_URL)
-    #content = json.loads(response.content.decode('utf-8'))
-    """
-    if response.status_code != 200:
-        return jsonify({
-            'status': 'error',
-            'message': 'La requete a l\'API REST(?) n\'a pas fonctionne. Voici le message renvoye par l\'API : {}'.format(content['message'])
-        }), 500
-    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
