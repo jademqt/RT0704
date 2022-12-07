b0ad807dc9ac429efb99b6aed56178ca551433c2
Jules Klein <jules.klein1@etudiant.univ-reims.fr>
2022-12-07 16:45:53 +0000

    improvement to run script

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+echo "launching REST server..."
 # launch rest server
-python3 rest/server/sv.py 2>log/log_rest &
+python3 rest/server/sv.py > log/log_rest 2>&1 &
+pid1=$!
 
+echo "launching web server..."
 #launch web server
-python3 web/app.py 2>log/web_log &
+python3 web/app.py > log/log_web 2>&1 &
+pid2=$!
+
+
+trap "echo killing servers...; kill ${pid1} ${pid2}; echo done.; exit" SIGINT SIGQUIT
+
+echo "press [Ctrl+C] to stop..."
+wait
